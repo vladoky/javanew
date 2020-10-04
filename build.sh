cd41669fdc0930813938c74cb98cb3d2604495c1
VDSevch <vladokyokydoky@mail.com>
2020-10-04 19:34:24 +0400

    lab7

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cd src/main/resources/keystore/local/
 kubectl create secret generic testservicesecret --from-literal=keystore_password=1234567890 --from-file=keystore.p12=keystore.p12 --from-literal=truststore_password=1234567890 --from-file=truststore.jks=truststore.jks
 
 cd ../../../../..
-sed "s%vladokyokydoky/testservice:ssl%$1%g" test-service-deployment.yaml > deployment.yaml
+sed "s%vladokyokydoky/testservice:ssl%vladokyokydoky/testservice:test%g" test-service-deployment.yaml > deployment.yaml
 kubectl apply -f deployment.yaml
 
 kubectl apply -f test-service-service.yaml
